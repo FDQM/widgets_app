9c6c481e51c2eccf6d9d74c586c8874390500256
FDQM <franklindavid.quezadamartinez@gmail.com>
2024-06-04 23:44:22 -0400

    DarkMode and theme changes using riverpod

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -14,13 +14,15 @@ const colorList = <Color>[
 
 class AppTheme {
   final int selectedColor;
+  final bool isDarkMode;
 
-  AppTheme({this.selectedColor = 0})
+  AppTheme( {this.isDarkMode = false,this.selectedColor = 0})
       : assert(selectedColor >= 0, 'Selected Color must be grater than 0'),
         assert(selectedColor < colorList.length,
             'Selected color must be less or equal than ${colorList.length - 1}');
 
   ThemeData geteTheme() => ThemeData(
+    brightness: isDarkMode?Brightness.dark:Brightness.light,
       colorSchemeSeed: colorList[selectedColor],
       appBarTheme: const AppBarTheme(centerTitle: false));
 }
